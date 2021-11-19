a14f0849aef45a928367224567cbc5a12ff88a98
cindylin622 <cindylin622@gmail.com>
2021-11-19 11:59:57 -0500

    Initial Commit

---
+++ FirstGit2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstGit2
+//
+//  Created by Cindy lin on 11/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
